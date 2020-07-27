1b767e8ca5d0b4600b3070f2072d135b9c0967e5
Aditya Mudgerikar <aditay.mudgerikar@msm.digital>
2020-07-27 12:08:37 +0530

    fees Model

--- src-ts/shared/model/DB/fees.DB.model.ts
+++ src-ts/shared/model/DB/fees.DB.model.ts
@@ -1,10 +1,14 @@
-
 // export class FeeGroup {
 //   feeGroupName: FeeGroupName = "";
 //   fees: Fee[] = [];
 // }
 
 import { GeneralDBItem, ObjectId } from "./imports/DB.model";
+
+export class FeesHead extends GeneralDBItem {
+  feesHeadName: string = "";
+  parentFeesHeadId: ObjectId = "";
+}
 
 export class FeeType {
   feeTypeName: FeeTypeName = "";
@@ -13,18 +17,18 @@ export class FeeType {
 export class Fee extends GeneralDBItem {
   instituteId: ObjectId = "";
   subjectCombinationId: ObjectId = "";
-  feeHeadName: FeesHeadName = "";
+  feeHeadId: ObjectId = "";
   feeTypeName: FeeTypeName = "";
   academicYear: string = "";
   modeOfpayment: FeesModeOfPayment = "MONTH";
-  amount: CourseAmount = new CourseAmount();
+  amount: FeeAmount = new FeeAmount();
   concession: FeesConcession = new FeesConcession();
   lateFine: FeesLateFine = new FeesLateFine();
   accountHeads: FeesAccountHead[] = [];
   concessionAccounts: FeesAccountHead[] = [];
 }
 
-export class CourseAmount {
+export class FeeAmount {
   nationals: FeeStudentCategory[] = [];
   foreign = 0;
 }
@@ -48,12 +52,12 @@ export class FeesLateFine {
   maxRevocableAmount = 10;
 }
 
-export class FeesAccountHead {
-  accountHeadId = "";
+export class FeesAccountHead extends GeneralDBItem {
+  accountHeadParentId: ObjectId = "";
   amountPercent = 0;
 }
 
 export type FeeTypeName = string; // Application Fees Exam Fees
-export type FeesHeadName = string; // Tution Fees etc
+// export type FeesHeadName = string; // Tution Fees etc
 export type FeesModeOfPayment = "SEMESTER" | "MONTH" | "YEAR";
 export type FeeGroupName = string;
