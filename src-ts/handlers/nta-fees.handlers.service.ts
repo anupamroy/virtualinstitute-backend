5e4aecaf397f5a7410c46d0c9db8c1945a3b62d1
AWS_CodeCommit-at-725755216375 <AWS_CodeCommit-at-725755216375>
2020-07-29 00:13:14 +0530

    Mock Functions

--- src-ts/handlers/nta-fees.handlers.service.ts
+++ src-ts/handlers/nta-fees.handlers.service.ts
@@ -22,3 +22,23 @@ export const putItem = async (event: APIGatewayProxyEvent) => {
   const result = await DynamoDBActions.putItem({ id, name });
   return createResponse(200, result);
 };
+
+// Actual Functions
+
+export const getFeeTypeById = async (event: APIGatewayProxyEvent) => {};
+export const getAccountHeadById = async (event: APIGatewayProxyEvent) => {};
+export const getFeesHeadById = async (event: APIGatewayProxyEvent) => {};
+
+export const getFeeTypeList = async (event: APIGatewayProxyEvent) => {};
+export const getAccountTypeList = async (event: APIGatewayProxyEvent) => {};
+export const getFeeHeadList = async (event: APIGatewayProxyEvent) => {};
+
+export const createFeeType = async (event: APIGatewayProxyEvent) => {};
+export const createAccountHead = async (event: APIGatewayProxyEvent) => {};
+export const createFeesHead = async (event: APIGatewayProxyEvent) => {};
+export const editFeeType = async (event: APIGatewayProxyEvent) => {};
+export const editAccountHead = async (event: APIGatewayProxyEvent) => {};
+export const editFeesHead = async (event: APIGatewayProxyEvent) => {};
+export const deleteFeeType = async (event: APIGatewayProxyEvent) => {};
+export const deleteAccountHead = async (event: APIGatewayProxyEvent) => {};
+export const deleteFeesHead = async (event: APIGatewayProxyEvent) => {};
