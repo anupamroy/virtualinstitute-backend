ffd715dba00fa9705178477f6ff2514283724b5d
AWS_CodeCommit-at-725755216375 <AWS_CodeCommit-at-725755216375>
2020-07-30 01:26:08 +0530

    sam-old to sam back

--- sam-old/nta-institute/src/shared/helpers/response.helper.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.optionsResponse = exports.keysMissingResponse = exports.unauthorisedAccessResponse = void 0;
-const handler_1 = require("./handler");
-const request_method_model_1 = require("../model/request-method.model");
-exports.unauthorisedAccessResponse = () => handler_1.createResponse(403, new request_method_model_1.APIResponse(true, "Unauthorised Access"));
-exports.keysMissingResponse = () => handler_1.createResponse(400, new request_method_model_1.APIResponse(true, "Some keys Missing"));
-exports.optionsResponse = () => handler_1.createResponse(200, null);
