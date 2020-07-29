d8d7e843a3efac0a04a9cf0e73d5cc787ea2a4e6
AWS_CodeCommit-at-725755216375 <AWS_CodeCommit-at-725755216375>
2020-07-30 01:34:47 +0530

    removed unnecessary files

--- scripts/build.clean.sh
+++
@@ -1,6 +0,0 @@
-cd sam;
-for d in */ ; do
-    cd $d
-    if [ -d src ]; then rm -rf src/*; fi
-    cd ..
-done
--- scripts/build.install_dep.sh
+++
@@ -1,7 +0,0 @@
-cd sam;
-for d in */ ; do
-    cd $d;
-    echo $d;
-    npm i;
-    cd ..
-done
