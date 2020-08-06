89e2dab13aa2f304e35fb5eb1ff3d80d46b08a69
Aditya Mudgerikar <aditya18thm@gmail.com>
2020-08-07 00:20:46 +0530

    Added tableType key to the GeneralDBItem

--- src-ts/shared/model/DB/imports/DB.model.ts
+++ src-ts/shared/model/DB/imports/DB.model.ts
@@ -1,5 +1,5 @@
-import { v4 as uuidv4 } from "uuid";
-import { ObjectId, TableName } from "./types.DB.model";
+import { v4 as uuidv4 } from 'uuid';
+import { ObjectId, TableName } from './types.DB.model';
 
 export class GeneralDBItem {
   id: ObjectId = uuidv4();
@@ -8,9 +8,9 @@ export class GeneralDBItem {
   // No data is deleted. This key decides whether or not to fetch this item.
   isActive: boolean = true;
   isDeleted: boolean = false;
-
-  created_by: ObjectId = "";
+  tableType: string = '';
+  created_by: ObjectId = '';
   created_at = new Date().toISOString();
-  updated_by: ObjectId = "";
+  updated_by: ObjectId = '';
   updated_at = new Date().toISOString();
 }
