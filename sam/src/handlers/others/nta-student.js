9df217318d5b8928e9a64ca5f6faaa068f41a964
Aditya Mudgerikar <aditya18thm@gmail.com>
2020-08-07 14:00:48 +0530

    gitignore working

--- sam/src/handlers/others/nta-student.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// import { getAllItems, getById, putItem } from "./nta-student.service";
-// const getAllItemsHandler = AWSHandler("GET", getAllItems);
-// const getByIdHandler = AWSHandler("GET", getById);
-// const putItemHandler = AWSHandler("POST", putItem);
-// export { getAllItemsHandler, getByIdHandler, putItemHandler };
