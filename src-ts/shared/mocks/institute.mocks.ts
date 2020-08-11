05f2885afa4ee721e063ca764b26e02d78dd160e
AWS_CodeCommit-at-725755216375 <AWS_CodeCommit-at-725755216375>
2020-08-12 01:02:39 +0530

    Mocks array

---
+++ src-ts/shared/mocks/institute.mocks.ts
@@ -0,0 +1,7 @@
+export const FeesGroupList = [];
+export const FeesMasterList = [];
+export const CourseFeesList = [];
+export const ConcessionApplicationList = [];
+export const lateFineApplicationList = [];
+export const studentFeesList = [];
+export const LedgerList = [];
