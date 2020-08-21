9db88ab64737f08c72b7d5889dcbafba8edbc503
AWS_CodeCommit-at-725755216375 <AWS_CodeCommit-at-725755216375>
2020-08-21 13:39:41 +0530

    S3 Get signed url instead of directly uploading

--- src-ts/shared/helpers/s3.handler.ts
+++ src-ts/shared/helpers/s3.handler.ts
@@ -15,3 +15,15 @@ export const uploadFileToS3 = async (Key: string, file: DecodedFile) => {
   };
   return await S3Object.upload(S3Params).promise();
 };
+
+export const getSignedUrlS3 = async (Key: string, ContentType: string) => {
+  // TODO: Insert Meta Data
+  const S3Params: S3.Types.PutObjectRequest = {
+    Bucket: S3_BUCKETS.PRIMARY,
+    Key,
+    ContentType,
+    ACL: 'public-read',
+  };
+  console.log('S3Params', S3Params);
+  return await S3Object.getSignedUrlPromise('putObject', S3Params);
+};
