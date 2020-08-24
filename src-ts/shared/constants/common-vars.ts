fec326cfbc850f0eb9ceb8c78f2253ec6a343499
Aditya Mudgerikar <aditya18thm@gmail.com>
2020-08-24 15:55:21 +0530

    Corrected the App Id's for Seller and Institute

--- src-ts/shared/constants/common-vars.ts
+++ src-ts/shared/constants/common-vars.ts
@@ -22,11 +22,11 @@ export const CORS_HEADERS = {
 
 export const CognitoConfig = {
   studentInstituteUserPoolId: "ap-south-1_GYBsglFix",
-  studentInstituteAppId: "12gn9davklo64a8rtn8mtvo2qv",
+  studentInstituteAppId: "66ruh82hd778bev05dtqp8ahlp",
   ntaUserPoolId: "ap-south-1_YOsP7cTUw",
   ntaAPIPasskey:
     "426b5e01-ba0f-42bf-8da9-2143f19ec57a-c909bce0-f3a4-4e67-8a3f-63958c77fdc8",
-  ntaAppId: "41m76ih5a4du31srs3qe0lnfg8",
+  ntaAppId: "59pt1p4ldhrceiv3qtkvthq88t",
 };
 
 export const REQUEST_HEADERS = {
