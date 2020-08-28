9e34004de2c68a422378e76b2cc61993584730e0
Aditya Mudgerikar <aditya18thm@gmail.com>
2020-08-28 23:07:42 +0530

    Moved the DynamoDb constants to the db-handler.ts

--- src-ts/shared/constants/common-vars.ts
+++ src-ts/shared/constants/common-vars.ts
@@ -1,94 +1,81 @@
-import * as aws from "aws-sdk";
+import * as aws from 'aws-sdk';
 // Create clients and set shared const values outside of the handler.
 
 // Create a DocumentClient that represents the query to add an item
-const dynamodb = require("aws-sdk/clients/dynamodb");
 
-export const DYNAMO_DB_URL = "";
-
-export const CommonItems = {
-  documentClient: new dynamodb.DocumentClient(),
-  // Get the DynamoDB table name from environment variables
-  studentTable: process.env.STUDENT_TABLE,
-  // institutetable: process.env.
-};
+export const DYNAMO_DB_URL = '';
 
 export const CORS_HEADERS = {
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "POST,GET,PATCH,PUT,DELETE",
-  // "Access-Control-Allow-Credentials": "true",
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'POST,GET,PATCH,PUT,DELETE',
 };
 
 export const CognitoConfig = {
-  studentInstituteUserPoolId: "ap-south-1_GYBsglFix",
-  studentInstituteAppId: "66ruh82hd778bev05dtqp8ahlp",
-  ntaUserPoolId: "ap-south-1_YOsP7cTUw",
+  studentInstituteUserPoolId: 'ap-south-1_GYBsglFix',
+  studentInstituteAppId: '66ruh82hd778bev05dtqp8ahlp',
+  ntaUserPoolId: 'ap-south-1_YOsP7cTUw',
   ntaAPIPasskey:
-    "426b5e01-ba0f-42bf-8da9-2143f19ec57a-c909bce0-f3a4-4e67-8a3f-63958c77fdc8",
-  ntaAppId: "59pt1p4ldhrceiv3qtkvthq88t",
+    '426b5e01-ba0f-42bf-8da9-2143f19ec57a-c909bce0-f3a4-4e67-8a3f-63958c77fdc8',
+  ntaAppId: '59pt1p4ldhrceiv3qtkvthq88t',
 };
 
 export const REQUEST_HEADERS = {
-  ntaAPIPasskey: "Nta-Api-Pass-Key",
+  ntaAPIPasskey: 'Nta-Api-Pass-Key',
 };
 
 export const USER_PASSWORD =
-  "e9dd7046-d1af-11ea-b33d-976e79865d9c-eaf73d4a-d1af-11ea-bb68-6350c5c6f354";
+  'e9dd7046-d1af-11ea-b33d-976e79865d9c-eaf73d4a-d1af-11ea-bb68-6350c5c6f354';
 
 export const TABLE_NAMES = {
-  // studentTable: 'StudentsTable',
-  instituteTable: "InstituteTableV1",
-  // feesTable: 'FeesTable',
-  // transactionsTable: 'TransactionsTable',
+  instituteTable: 'InstituteTableV1',
+  globalMastersTable: 'GlobalMastersTable',
 };
-
-export const NTA_MASTER_SET_ID = "debe7d4a-d29a-11ea-b256-073ae46f3a1a";
 
 export const cognito = new aws.CognitoIdentityServiceProvider();
 
 export const EVENT_HEADERS = {
-  accessToken: "access-token",
-  ntaAuthorityId: "nta-authority-id",
+  accessToken: 'access-token',
+  ntaAuthorityId: 'nta-authority-id',
 };
 
 export const EVENT_HEADERS_LOCAL = {
-  accessToken: "Access-Token",
-  ntaAuthorityId: "Nta-Authority-Id",
+  accessToken: 'Access-Token',
+  ntaAuthorityId: 'Nta-Authority-Id',
 };
 
 export const ERRORS = {
-  INSTITUTION_TYPE_NO_EXIST: "Institution Type Does not exist",
-  PARENT_FEES_HEAD_NOT_EXIST: "Parent Fees head Does not exist",
+  INSTITUTION_TYPE_NO_EXIST: 'Institution Type Does not exist',
+  PARENT_FEES_HEAD_NOT_EXIST: 'Parent Fees head Does not exist',
   FEES_HEAD_NO_SPECIAL_CHARS:
-    "Fees head name should not contain any special characters.",
-  FEES_HEAD_NAME_ALREADY_EXISTS: "Fees head name already exists",
-  FEES_HEAD_NO_EXISTS: "Fees head does not Exist",
+    'Fees head name should not contain any special characters.',
+  FEES_HEAD_NAME_ALREADY_EXISTS: 'Fees head name already exists',
+  FEES_HEAD_NO_EXISTS: 'Fees head does not Exist',
   FEES_HEAD_INSTITUTE_ID_NO_MATCH_PARENT:
-    "The Fees Head Institute does not match with the parent Fees head",
+    'The Fees Head Institute does not match with the parent Fees head',
   FEES_TYPE_NO_SPECIAL_CHARS:
-    "Fees Type name should not contain any special characters.",
-  FEES_TYPE_NO_EXIST: "Fees Type does not Exist",
-  FEES_TYPE_NAME_ALREADY_EXISTS: "Fees type name already exists",
-  PARENT_ACCOUNT_HEAD_NO_EXISTS: "Parent Accounts head Does not exist",
+    'Fees Type name should not contain any special characters.',
+  FEES_TYPE_NO_EXIST: 'Fees Type does not Exist',
+  FEES_TYPE_NAME_ALREADY_EXISTS: 'Fees type name already exists',
+  PARENT_ACCOUNT_HEAD_NO_EXISTS: 'Parent Accounts head Does not exist',
   ACCOUNTS_HEAD_NO_SPECIAL_CHARS:
-    "Accounts head name should not contain any special characters.",
-  ACCOUNTS_HEAD_NO_EXISTS: "Accounts head does not Exist",
-  ACCOUNTS_HEAD_NAME_ALREADY_EXISTS: "Fees type name already exists",
-  GENERAL_UNAUTHORISED_ACCESS: "Unauthorised Access",
-  GENERAL_KEYS_MISSING: "Some keys Missing",
+    'Accounts head name should not contain any special characters.',
+  ACCOUNTS_HEAD_NO_EXISTS: 'Accounts head does not Exist',
+  ACCOUNTS_HEAD_NAME_ALREADY_EXISTS: 'Fees type name already exists',
+  GENERAL_UNAUTHORISED_ACCESS: 'Unauthorised Access',
+  GENERAL_KEYS_MISSING: 'Some keys Missing',
 };
 
 export const S3_BUCKETS = {
-  PRIMARY: "virtualinstitutestorage",
+  PRIMARY: 'virtualinstitutestorage',
 };
 
 export const S3_FOLDER_STRUCTURE = {
-  ORGANIZATION: "org_meta",
-  IMAGES: "/images",
-  REGISTRATION_DOCUMENTS: "/registration-documents",
-  AFFILIATION_DOCUMENTS: "/affiliation-documents",
-  PROFILE_META: "/profile_meta",
-  PROFILE_PICTURE: "/profile-picture",
-  LOGO: "/logo",
+  ORGANIZATION: 'org_meta',
+  IMAGES: '/images',
+  REGISTRATION_DOCUMENTS: '/registration-documents',
+  AFFILIATION_DOCUMENTS: '/affiliation-documents',
+  PROFILE_META: '/profile_meta',
+  PROFILE_PICTURE: '/profile-picture',
+  LOGO: '/logo',
 };
--- src-ts/shared/helpers/db-handler.ts
+++ src-ts/shared/helpers/db-handler.ts
@@ -1,44 +1,51 @@
-import { CommonItems, TABLE_NAMES } from '../constants/common-vars';
-import { createResponse } from "./handler-common";
-import { APIResponse } from "../model/request-method.model";
+import { TABLE_NAMES } from '../constants/common-vars';
+import { createResponse } from './handler-common';
+import { APIResponse } from '../model/request-method.model';
+import { TableName } from '../model/DB/imports/types.DB.model';
+
+const dynamodb = require('aws-sdk/clients/dynamodb');
 
 // import { documentClient } from "../constants/common-vars";
-import { TableName } from "../model/DB/imports/types.DB.model";
+const documentClient = new dynamodb.DocumentClient();
+
 export const DynamoDBActions = {
   putItem: (Item: any, TableName = TABLE_NAMES.instituteTable): Promise<any> =>
-    CommonItems.documentClient.put({ TableName, Item }).promise(),
+    documentClient.put({ TableName, Item }).promise(),
   get: (Key: any, TableName = TABLE_NAMES.instituteTable): Promise<any> =>
-    CommonItems.documentClient.get({ TableName, Key }).promise(),
-  getItemById: (id: string, tableType: TableName, TableName = TABLE_NAMES.instituteTable) =>
-    CommonItems.documentClient
+    documentClient.get({ TableName, Key }).promise(),
+  getItemById: (
+    id: string,
+    tableType: TableName,
+    TableName = TABLE_NAMES.instituteTable
+  ) =>
+    documentClient
       .query({
         TableName,
-        KeyConditionExpression: "id = :id and tableType = :tableType",
+        KeyConditionExpression: 'id = :id and tableType = :tableType',
         ExpressionAttributeValues: {
-          ":id": id,
-          ":tableType": tableType,
+          ':id': id,
+          ':tableType': tableType,
         },
       })
       .promise()
       .then((result: { Items: any[] }) => result.Items[0]),
   scan: (TableName: string, params?: any): Promise<any> =>
-    CommonItems.documentClient.scan({ TableName, ...params }).promise(),
+    documentClient.scan({ TableName, ...params }).promise(),
   batchGet: (params: any): Promise<any> =>
-    CommonItems.documentClient.batchGet({ RequestItems: params }).promise(),
-  query: (params: any): Promise<any> =>
-    CommonItems.documentClient.query(params).promise(),
+    documentClient.batchGet({ RequestItems: params }).promise(),
+  query: (params: any): Promise<any> => documentClient.query(params).promise(),
   delete: (params: any, TableName: string) =>
-    CommonItems.documentClient.delete({ TableName, ...params }).promise(),
+    documentClient.delete({ TableName, ...params }).promise(),
 };
 
 export const processDynamoDBResponse = (event: Promise<any>, data?: any) =>
   event
     .then((response) =>
-      createResponse(200, new APIResponse(false, "", data || response))
+      createResponse(200, new APIResponse(false, '', data || response))
     )
     .catch((error) =>
       createResponse(
         422,
-        new APIResponse(false, error.message || "An Error Occured", error)
+        new APIResponse(false, error.message || 'An Error Occured', error)
       )
     );
